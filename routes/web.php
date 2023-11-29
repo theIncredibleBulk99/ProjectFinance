90575d42a1d294c1e8562d71c75915e096983ae3
damahindra <ranggaandhito@gmail.com>
2023-11-29 22:20:37 +0700

    completed analytical dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,7 @@ use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\AnalyticsController;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\nggejson;
->>>>>>> 38bcd0ce88ebdf6801661d0e424b7a8e8ee37bb5
 use ArielMejiaDev\LarapexCharts\Facades\LarapexChart;
 use Illuminate\Support\Facades\Route;
 
