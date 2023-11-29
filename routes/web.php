06d3a98bb96e43ad0bfcda0cbbfa1402297d7962
damahindra <ranggaandhito@gmail.com>
2023-11-29 17:10:49 +0700

    optimized home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HistoryController;
 use ArielMejiaDev\LarapexCharts\Facades\LarapexChart;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/home', [HomeController::class, 'index'])->name('home.show');
+Route::get('/history', [HistoryController::class, 'index'])->name('history.show');
 
 Route::get('chart', function () {
     $chart = (new LarapexChart)::setType('line')
