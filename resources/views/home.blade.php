009afd2e73f5da90b6c296c8d086535cb5383c7d
damahindra <ranggaandhito@gmail.com>
2023-11-29 11:42:10 +0700

    added template view for navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('template')
+
+@section('content')
+    <!-- Your view content goes here -->
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
 
 Route::get('chart', function () {
